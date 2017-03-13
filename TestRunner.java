6d75859f0b9a8463818b1cadc562150941169064
Jason Ly <jal192@pitt.edu>
2017-03-13 12:01:43 -0400

    Added cathedral pics test to list

--- TestRunner.java
+++ TestRunner.java
@@ -14,7 +14,8 @@ public class TestRunner {
 	//classesToTest.add(HomeTest.class);
 	//classesToTest.add(FactorialTest.class);
 	//classesToTest.add(FibonacciTest.class);
-	classesToTest.add(HelloTest.class);
+	//classesToTest.add(HelloTest.class);
+	classesToTest.add(CathedralPicsTest.class);
 	
 	// For all test classes added, loop through and use JUnit
 	// to run them.
